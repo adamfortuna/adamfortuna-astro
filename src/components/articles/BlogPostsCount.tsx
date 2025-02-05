504b39480af7818f091ca479840c5e19dedb6635
Adam Fortuna <adam@fortuna.name>
2025-02-05 10:47:37 -0700

    Basic blog page

--- src/components/articles/BlogPostsCount.tsx
+++
@@ -1,8 +0,0 @@
-import { getPostsCount } from '@/queries/wordpress/getPostsCount'
-
-const BlogPostsCount = async (): Promise<React.ReactElement> => {
-  const postsCount = (await getPostsCount()) || 624
-  return <span>{postsCount.toLocaleString()}</span>
-}
-
-export default BlogPostsCount
