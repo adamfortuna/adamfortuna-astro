ebbebc608e303de28dd66b467a7a3c100140d886
Adam Fortuna <adam@fortuna.name>
2025-02-09 14:30:26 -0700

    Fix webhook url for netlify

--- src/pages/api/webhook.json.ts
+++ src/pages/api/webhook.json.ts
@@ -16,30 +16,39 @@ function sourceToProject(sourceUrl: string): WordpressClientIdentifier | null {
 }
 
 export async function POST({ request }: { request: Request }) {
-  const body = await request.json();
-  const { post_id } = body;
-  const postTags = Object.keys(body.taxonomies?.post_tag || {});
+  try {    
+    // See below for information on webhook security
+    if (request.headers.get("x-wordpress-webhook-secret") !== import.meta.env.WORDPRESS_WEBHOOK_SECRET) {
+      return new Response("Unauthorized", { status: 401 });
+    }
 
-  if(!post_id) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+    const body = await request.json();
+    const { post_id } = body;    
+    if(!post_id) {
+      return new Response("No Post ID", { status: 401 });
+    }
 
-  // See below for information on webhook security
-  if (request.headers.get("x-wordpress-webhook-secret") !== import.meta.env.WORDPRESS_WEBHOOK_SECRET) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-  const project = sourceToProject(request.headers.get("x-wp-webhook-source") || "");
-  if(!project) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-  const tags = [
-    `post-id-${body.post_id}`,
-    `blog/projects/${project}`,
-    'blog',
-    'blog/all',
-    ...postTags
-  ]
-  await purgeCache({ tags });
+    const project = sourceToProject(request.headers.get("x-wp-webhook-source") || "");
+    if(!project) {
+      return new Response("No Project", { status: 401 });
+    }
 
-  return new Response(`Revalidated entry with id ${body.sys.id}`, { status: 200 });
+    const postTags = Object.keys(body.taxonomies?.post_tag || {});
+    const tags = [
+      `post-id-${post_id}`,
+      `blog/projects/${project}`,
+      'blog',
+      'blog/all',
+      ...postTags
+    ]
+    await purgeCache({
+      siteID: import.meta.env.NETLIFY_SITE_ID,
+      tags,
+      token: import.meta.env.NETLIFY_TOKEN
+    });
+  
+    return new Response(`Revalidated entry with id ${post_id}`, { status: 200 });
+  } catch(e) {
+    return new Response(`Something went wrong: ${e}`, { status: 500 });
+  }
 }
