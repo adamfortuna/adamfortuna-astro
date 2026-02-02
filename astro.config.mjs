9645183ef60827393d851a8e8d59a9790ba7a7a2
Adam Fortuna <adam@fortuna.name>
2026-02-01 21:35:16 -0700

    Reset middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,29 @@ import icon from "astro-icon";
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
+// MessageChannel polyfill for React SSR on Cloudflare Workers
+const messageChannelPolyfill = `
+if (typeof MessageChannel === 'undefined') {
+  globalThis.MessageChannel = class MessageChannel {
+    constructor() {
+      this.port1 = { postMessage: (d) => queueMicrotask(() => this.port2.onmessage?.({ data: d })), onmessage: null };
+      this.port2 = { postMessage: (d) => queueMicrotask(() => this.port1.onmessage?.({ data: d })), onmessage: null };
+    }
+  };
+}
+`;
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          banner: messageChannelPolyfill,
+        },
+      },
+    },
   },
 
   integrations: [icon(), react()],
